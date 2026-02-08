4d6e357807d7dd69a3be2ffc4c75a97dc761ddb8
omar.elkhotri <omar.elkhotri@vmg-conseil.com>
2026-02-08 19:07:58 +0100

    Init Project + docker

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { onMounted, ref } from 'vue';
+  const apiResponse = ref(null)
+
+  onMounted(async () => {
+    const res = await fetch("http://127.0.0.1:8000/api/ping")
+    apiResponse.value = await res.json()
+  })
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Response : {{ apiResponse }}
+  </p>
+</template>
+
+<style scoped></style>
